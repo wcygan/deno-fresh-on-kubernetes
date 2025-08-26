c9d48b7cf64d20ad9e98aca8dabdcea93558a7d2
Will Cygan <wcygan.io@gmail.com>
2025-08-25 20:28:43 -0500

    naming

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Deno K8S',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -59,9 +59,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Fresh on k8s',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Fresh on k8s Logo',
         src: 'img/logo.svg',
       },
       items: [
